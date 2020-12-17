3395aa3b01eab30001afd63a3a814dbb37461e1f
Farid Ali <faridalicode@gmail.com>
2020-12-17 21:21:13 +0530

    Display lagislators name by zipcode using google civic information

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,18 +1,36 @@
 require "csv"
+require "google/apis/civicinfo_v2"
 
 def clean_zipcode(zipcode)
   zipcode = zipcode.to_s.rjust(5, "0")[0..4]
 end
 
-puts "Event Manager Initialized!"
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+                                address: zipcode,
+                                levels: "country",
+                                roles: ["legislatorUpperBody", "legislatorLowerBody"])
+    legislators = legislators.officials
+    legislators_name = legislators.to_a.map(&:name)
+    legislators_name.join(", ")
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+end
+
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
 contents.each do |row|
   first_name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-
-  puts "#{first_name} : #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+  
+  puts "#{first_name}(#{zipcode}) : #{legislators}"
 end
 
 
