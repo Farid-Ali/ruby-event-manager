4071c05e557bd6c628d8f6c4c1a1b18bb14a7383
Farid Ali <faridalicode@gmail.com>
2020-12-20 21:53:39 +0530

    implemented method whic give us the hours when most people register

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -43,28 +43,51 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
+def hour_of_the_day_when_user_registered(registration_date_time)
+  #create ruby date time object
+  registration_date_time_object = DateTime.strptime(registration_date_time, "%m/%d/%y %H:%M")
+
+  #format date time object(There are no use case for this now may be use it later)
+  #registration_date_time = registration_date_time.strftime("%Y,%m,%d,%k,%M,%S")
+
+  #find the hour of the day when user register
+  registration_hour = registration_date_time_object.hour
+end
+
+def no_of_user_registered_in_a_specific_hour(registration_hours)
+  while registration_hours.length > 0
+    #count the user registered in a specfic hour
+    registration_counter = registration_hours.count(registration_hours.first)
+    puts "User registered at #{registration_hours.first}: #{registration_counter}"
+  
+    registration_hours = registration_hours.select { |a| a != registration_hours.first }
+  end
+end
+
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
 template_latter = File.read "form_letter.erb"
 erb_letter_template = ERB.new template_latter
 
+#Save the hour when user register in an Array
+registration_hours = Array.new
+
 contents.each do |row|
   id = row[0]
   first_name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   phone = row[:homephone]
+  
+  #insert registration_hour when user registered in registration_hours Array
+  registration_hours << hour_of_the_day_when_user_registered(row[:regdate])
+  
   legislators = legislators_by_zipcode(zipcode)
   phone_number = clean_phone_number(phone)
 
   form_letter = erb_letter_template.result(binding)
-
   save_thank_you_letter(id, form_letter)
-
-  #puts phone_number
-  #puts form_letter
-  
-  #puts "#{first_name}(#{zipcode}) : #{legislators}"
 end
 
 
+no_of_user_registered_in_a_specific_hour(registration_hours)
